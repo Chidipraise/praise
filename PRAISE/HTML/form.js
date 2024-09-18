561ddb3640efdaa81b81d96f53e6756db6a34ba6
Chidipraise <praisechidi99@gmail.com>
2024-09-18 06:38:51 +0100

    Add files via upload
    same text

---
+++ PRAISE/HTML/form.js
@@ -0,0 +1,45 @@
+// function validate(){
+//     const firstName = document.forms["myForm"]['fname'].value;
+//     if (firstName === '') {
+//         alert('Enter your first name');
+//         return false;
+//     }
+// }
+
+// function validateNumber(){
+//     let x = document.getElementById('myAge')
+//     if(isNaN(x)|| x<1 || x>10) {
+//         alert('Age must be greater than one and let than less than ten');
+
+//     } else alert('Enter the right age')
+
+    
+// }
+// const h1 = document.getElementById('myWorld');
+// const h2 = h1.textContent.toUpperCase();  // Convert the text content to uppercase
+
+// function myName() {
+//     alert(h2);  // Output the uppercase string
+// }
+
+// const p = document.getElementById('demo');
+// const h3 = p.textContent.toUpperCase();
+// const p2 = document.getElementById('demo2')
+
+// function paragraph() {
+//     // alert(h3);  // Output the uppercase string
+//     p2.innerText = h3
+// }
+
+// let a = 'enter a text'
+// const p = document.getElementById('DEMO');
+// const h1 = document.getElementById('NAME')
+// const h3 = a.toUpperCase();
+
+
+// function container() {
+//     h1.innerText = a
+//     p.innerText = h3
+// }
+
+
